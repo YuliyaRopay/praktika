5226bf42a8a4a36fe5abc58d697ca1046910a2bf
YuliyaRopay <jcolor@ukr.net>
2015-12-12 13:39:32 +0200

    day7_linkedlist

--- day7/Frame9MyLinkedList/Launcher.java
+++ day7/Frame9MyLinkedList/Launcher.java
@@ -4,32 +4,52 @@ import java.util.Iterator;
 
 public class Launcher {
     public static void main(String[] args) {
-        SimpleLinkedList simpleLinkedList=new SimpleLinkedList();
+
+        SimpleLinkedList simpleLinkedList =new SimpleLinkedList();
+
+        System.out.println(">>> add first");
         simpleLinkedList.addFirst(1);
         simpleLinkedList.addFirst(2);
         simpleLinkedList.addFirst(3);
-        simpleLinkedList.printAll();
+        simpleLinkedList.addFirst(4);
+        System.out.println(simpleLinkedList.toString());
 
-        System.out.println("-->>>");
-        simpleLinkedList.addLast(4);
-        simpleLinkedList.addLast(5);
-        simpleLinkedList.printAll();
+        System.out.println();
+        System.out.println(">>> add last");
+        simpleLinkedList.addLast("a");
+        simpleLinkedList.addLast("b");
+        simpleLinkedList.addLast("c");
+        simpleLinkedList.addLast("d");
+        System.out.println(simpleLinkedList.toString());
 
-        System.out.println("-->>>");
-        simpleLinkedList.addAfter(0,3);
-        simpleLinkedList.printAll();
+        System.out.println();
+        System.out.println(">>> add after");
+        simpleLinkedList.addAfter("k", "q");
+        simpleLinkedList.addAfter("f","a");
+        simpleLinkedList.addAfter("c","c");
+        simpleLinkedList.addAfter("s",4);
+        simpleLinkedList.addAfter("dd", "d");
+        simpleLinkedList.addAfter("h", "dd");
+        System.out.println(simpleLinkedList.toString());
 
-
-        System.out.println("-->>>iterator");
-        for(Object o: simpleLinkedList){
-            Integer integer=(Integer)o;
-            System.out.println(integer);
+        System.out.println();
+        System.out.println(">>> iterator");
+        for(Object o:simpleLinkedList){
+            System.out.println(o.toString());
         }
 
-        System.out.println("-->>>iterator2");
-        for(Iterator<Object> it =simpleLinkedList.iterator();it.hasNext();){
+
+        System.out.println();
+        System.out.println("-->>>iterator remove a, c");
+        for(Iterator<Object> it = simpleLinkedList.iterator();it.hasNext();){
             Object o =it.next();
-            System.out.println(o);
+            if(o.equals("a") || o.equals("c")){
+                it.remove();
+            }
         }
+        Iterator<Object> it2 = simpleLinkedList.iterator();
+        do{
+            System.out.println(it2.next());
+        }while(it2.hasNext());
     }
 }
--- day7/Frame9MyLinkedList/SimpleLinkedList.java
+++ day7/Frame9MyLinkedList/SimpleLinkedList.java
@@ -1,14 +1,13 @@
 package day7.Frame9MyLinkedList;
 
-
 import java.util.Iterator;
 
-public class SimpleLinkedList implements Iterable<Object>
-{
+public class SimpleLinkedList implements Iterable<Object>{
+
     private Node nodeRoot;
     private int size;
 
-    public SimpleLinkedList(){
+    public SimpleLinkedList() {
         size=0;
         nodeRoot=null;
     }
@@ -18,7 +17,16 @@ public class SimpleLinkedList implements Iterable<Object>
         this.addFirst(object);
     }
 
+    //
+    public Node getNodeRoot() {
+        return nodeRoot;
+    }
 
+    public int getSize() {
+        return size;
+    }
+
+    //
     public void addFirst(Object object){
         if(object!=null){
             Node nodeTmp=new Node();
@@ -32,90 +40,79 @@ public class SimpleLinkedList implements Iterable<Object>
             nodeRoot=nodeTmp;
             addSize();
         }
-
-        /*
-        Node n=new Node();
-        n.object=object;
-        if(nodeRoot!=null){
-            n.ref=nodeRoot;
-
-        }
-        nodeRoot=n;
-        size++;
-        */
     }
 
+    //
     public void addLast(Object object){
         if(object!=null){
             Node nodeTmp=new Node();
             nodeTmp.object=object;
-            if(nodeRoot==null){
+            Node nodeLast=getLastNode();
+            if(nodeLast==null){
                 nodeRoot=nodeTmp;
             }
             else{
-                Node nodeLast=nodeRoot;
-                Node nodeRef=nodeRoot;
-                while (nodeRef.ref!=null){
-                    nodeLast=nodeRef;
-                    nodeRef=nodeRef.ref;
-                }
-                nodeRef.ref=nodeTmp;
+                nodeLast.ref=nodeTmp;
             }
             addSize();
         }
     }
 
+
+    //
     public void addAfter(Object object, Object prev){
-        Node nodePrev=null;
-        Node nodeCurrent=nodeRoot;
-        do{
-            if(nodeCurrent.object==prev){
-                nodePrev=nodeCurrent;
-                break;
+        if(object!=null){
+            Node nodeTmp=new Node();
+            nodeTmp.object=object;
+            Node nodePrev=getNode(prev);
+            if(nodePrev!=null){
+                nodeTmp.ref=nodePrev.ref;
+                nodePrev.ref=nodeTmp;
+                addSize();
             }
-            nodeCurrent=nodeCurrent.ref;
-        }while(nodeCurrent!=null && nodeCurrent.ref!=null);
-
-        if(nodePrev==null){
-            throw new IllegalStateException("List doesn't contain prev object.");
         }
-        Node nodeTmp=new Node();
-        nodeTmp.object=object;
-
-        if(nodePrev.ref!=null){
-            nodeTmp.ref=nodePrev.ref;
-        }
-
-        nodePrev.ref=nodeTmp;
-        addSize();
-
     }
 
+
+
+    //
+    private Node getLastNode(){
+        Node node=nodeRoot;
+        Node nodeTmp=nodeRoot;
+        while (node!=null){
+            nodeTmp=node;
+            node=node.ref;
+        }
+        return nodeTmp;
+    }
+
+    //
+    private Node getNode(Object object){
+        Node node=nodeRoot;
+        while (node!=null){
+            if(node.object.equals(object)){
+                return node;
+            }
+            node=node.ref;
+        }
+        return null;
+    }
+
+    //
     private void addSize(){
         size++;
     }
 
-    private void subtractSize(){
-        size--;
-    }
-
-    public int getSize(){
-        return size;
-    }
-
-
-    public void printAll(){
-        if(size==0){
-            System.out.println("Empty");
-        }
-
+    //
+    @Override
+    public String toString() {
+        String resultString="";
         Node node=nodeRoot;
-        while (node.ref!=null){
-            System.out.println(node.object+"->"+node.ref.object);
+        while(node!=null){
+            resultString=resultString+"["+node.object.toString()+"]";
             node=node.ref;
         }
-        System.out.println(node.object);
-
+        return resultString;
     }
 
     @Override
@@ -123,62 +120,68 @@ public class SimpleLinkedList implements Iterable<Object>
         return new SLLIterator();
     }
 
-
     //
     private class Node {
         private Object object;
         private Node ref;
 
-     }
+    }
 
     //
     private class SLLIterator implements Iterator<Object>{
 
-        private Node cp;
-        private Node prev;
+        Node nodeCurrent;
+        Node nodePrev;
 
         public SLLIterator() {
+            nodeCurrent=nodeRoot;
+            nodePrev=null;
         }
 
         @Override
         public boolean hasNext() {
-            return (cp==null && nodeRoot!=null)||(cp!=null && cp.ref!=null);
+            if(nodeCurrent.ref!=null){
+                return true;
+            }
+            return false;
         }
 
         @Override
         public Object next() {
-            if(cp==null && nodeRoot!=null){
-                cp=nodeRoot;
-                return cp.object;
-            }
+
             if(hasNext()){
-                cp=cp.ref;
-                return cp.object;
+                nodePrev=nodeCurrent;
+                nodeCurrent=nodeCurrent.ref;
+                return nodeCurrent.object;
             }
-            throw new IllegalStateException("List has no more elements");
+            throw new IllegalStateException("No more elements");
         }
 
         @Override
         public void remove() {
-            if(!hasNext()&&prev==null){
-                //only one element
-                cp=null;
-                nodeRoot=null;
-            }else if(!hasNext()&&prev!=null){
-                //last element
-                prev.ref=null;
-                cp=null;
-            }else if(hasNext()&&prev==null){
-                //first
-                nodeRoot=cp.ref;
-                cp=nodeRoot;
-            }else{
-                //middle
-                prev.ref=cp.ref;
-                cp=cp.ref;
+            if(nodeCurrent!=null){
+                if(nodePrev==null && !hasNext()){
+                    nodeCurrent=null;
+                    nodeRoot=null;
+                }
+                else if(nodePrev!=null && !hasNext()){
+                    nodePrev.ref=null;
+                    nodeCurrent=null;
+                }
+                else if(nodePrev==null && hasNext()){
+                    nodeRoot=nodeCurrent.ref;
+                    nodeCurrent=nodeRoot;
+                }
+                else{
+                    //System.out.println(">>>> "+nodeCurrent.object.toString());
+                    nodePrev.ref=nodeCurrent.ref;
+                    nodeCurrent=nodePrev;
+                    //nodeCurrent=nodeCurrent.ref;
+                    //System.out.println(">>>> nodePrev "+nodePrev.object.toString());
+                    //System.out.println(">>>> nodePrev ref "+ nodePrev.ref.object.toString());
+                    //System.out.println(">>>> nodeCurrent "+nodeCurrent.object.toString());
+                }
             }
-            size--;
-
         }
     }
 }
