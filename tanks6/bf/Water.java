a5586ea83920e22f1f88f762d10102a6ccf995cb
YuliyaRopay <jcolor@ukr.net>
2016-01-04 22:21:09 +0200

    day8_water

--- tanks6/bf/Water.java
+++ tanks6/bf/Water.java
@@ -12,4 +12,16 @@ public class Water extends SimpleBFObject{
         color = new Color(100,50, 255);
         setImage(BFImages.IMG_WATER);
     }
+
+    @Override
+    public void draw(Graphics g) {
+        Graphics2D g2D=(Graphics2D) g;
+        Composite origin=g2D.getComposite();
+        Composite transparent=AlphaComposite.getInstance(AlphaComposite.SRC_OVER,0.5f);
+        g2D.setComposite(transparent);
+        g.setColor(this.color);
+        g.fillRect(this.getX(), this.getY(), 64, 64);
+        //g.drawImage(image, getX(), getY(), null);
+        g2D.setComposite(origin);
+    }
 }
